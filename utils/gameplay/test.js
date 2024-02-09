b9a315cb5d4bd4979fe41fbe569c8985428c4b25
Psiko23 <tylerpittman2001@gmail.com>
2024-02-08 22:12:59 -0800

    pulling master

--- utils/gameplay/test.js
+++ utils/gameplay/test.js
@@ -28,7 +28,7 @@ class Game {
     async createDeck() {
         try{
             const allcards = await Card.findAll({
-                attributes: ['value', 'card_name', 'filename'],
+                attributes: ['value', 'cardName', 'filename'],
             });
             // console.log(allcards)
             const formattedCards = allcards.map((card) => card.get({ plain: true }));
